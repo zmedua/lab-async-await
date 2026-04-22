81633d90fe12d69758c74af7be2b6fd06776fe01
Zane <zimedua@gmail.com>
2026-04-22 16:40:33 -0500

    lab async

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here!
+const postList = document.getElementById('post-list');
+
+function displayPosts(posts) 
+{
+    posts.forEach(post => {
+    const li = document.createElement('li');
+    const h1 = document.createElement('h1');
+    const p = document.createElement('p');
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+    });
+}
+
+async function getPosts()
+{
+    
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await response.json();
+        displayPosts(posts);
+        return posts;
+
+}
+
+getPosts();
